cd727593f41d2033444f18f06a7e79407b83a21f
GrozniyToaster <ivan-nedd@mail.ru>
2021-06-23 14:54:32 +0300

    args

--- lzw.cpp
+++ lzw.cpp
@@ -6,7 +6,8 @@
 
 using namespace std;
 
-const uint16_t UPPER_BORDER = UINT16_MAX;
+const size_t UPPER_BORDER = UINT16_MAX;
+const size_t DEFAULT_DICT_SIZE = UINT8_MAX + 1;
 
 
 class Trie {
@@ -39,11 +40,11 @@ public:
             return true;
         }
     }
-    void  add (uint8_t ch, uint16_t code){
+    void  add (uint8_t ch, uint16_t code) const {
         st.cur_node->next[ch] = make_unique<Node>(code);
     }
 
-    uint16_t get_code (){
+    [[nodiscard]] uint16_t get_code () const noexcept{
         return st.cur_node->code;
     }
     void state_to_root(){
@@ -51,79 +52,52 @@ public:
     }
 
 };
-/*
-unordered_map<string, uint16_t> encode_dict() {
-    unordered_map<string, uint16_t> dict;
-    uint16_t count = 0;
-    for (unsigned char i = 0; i < UINT8_MAX; i++) {
-        dict[string(1, i)] = count;
-        count++;
-    }
-    dict[string(1, uint8_t(UINT8_MAX))] = count;
-
-    // dict[string(1, '\0')] = EOF_CODE;
-    return dict;
-}
-*/
 Trie encode_dict() {
     Trie dict;
-    for (uint8_t i = 0; i < UINT8_MAX; i++) {
+    for (uint16_t i = 0; i <= UINT8_MAX; i++) {
         dict.add(i, i);
     }
-    dict.add(UINT8_MAX, UINT8_MAX);
     return dict;
 }
 
 void LZW::encode(istream &input, ostream &out) {
     auto dict = encode_dict();
-    uint16_t count = UINT8_MAX + 1;
+    size_t count = DEFAULT_DICT_SIZE;
     char cur_ch;
     if (!read_raw_var(input, cur_ch)) {
-        //cur_ch = '\0';
         return;
     }
-    // string cur_str(1, cur_ch);
     dict.step(cur_ch);
     while (true) {
-        bool to_break = false;
         if (!read_raw_var(input, cur_ch)) {
             break;
         }
         if (dict.step(cur_ch)) {
-            //cur_str += cur_ch;
             continue;
         } else {
             write_raw_var(out, dict.get_code());
-            //dict[cur_str + cur_ch] = count;
             dict.add(cur_ch, count);
             count++;
             if (count == UPPER_BORDER) {
                 dict = encode_dict();
-                count = UINT8_MAX + 1;
+                count = DEFAULT_DICT_SIZE;
             }
-            //cur_str = cur_ch;
             dict.state_to_root();
             dict.step(cur_ch);
         }
     }
     write_raw_var(out, dict.get_code());
 }
-
-unordered_map<uint16_t, string> decode_dict() {
-    uint16_t count = 0;
-    unordered_map<uint16_t, string> dict;
-    for (unsigned char i = 0; i < UINT8_MAX; i++) {
-        dict[count] = i;
-        count++;
+vector<string> decode_dict() {
+    vector<string> dict (DEFAULT_DICT_SIZE);
+    for (uint16_t i = 0; i <= UINT8_MAX; i++) {
+        dict[i] = static_cast<char>(i);
     }
-    dict[count] = UINT8_MAX;
-    // dict[EOF_CODE] = '\0';
     return dict;
 }
-
 void LZW::decode(istream &input, ostream &out) {
     auto dict = decode_dict();
-    uint16_t count = dict.size();
+    size_t count = dict.size();
 
     uint16_t old_code;
     if (!read_raw_var(input, old_code)) return;
@@ -133,7 +107,7 @@ void LZW::decode(istream &input, ostream &out) {
     string str;
     while (read_raw_var(input, new_code)) {
 
-        if (!dict.count(new_code)) {
+        if (count <= new_code) {
             str = dict[old_code];
             str += symbol;
 
@@ -142,7 +116,7 @@ void LZW::decode(istream &input, ostream &out) {
         }
         write_raw_var(out, str);
         symbol = str[0];
-        dict[count] = dict[old_code] + symbol;
+        dict.emplace_back(dict[old_code] + symbol);
         count++;
         if (count == UPPER_BORDER) {
             dict = decode_dict();
--- main.cpp
+++ main.cpp
@@ -1,23 +1,138 @@
-//
-// Created by ivan- on 08.05.2021.
-//
 #include "huffman.hpp"
 #include "lzw.hpp"
 #include <iostream>
 #include <sstream>
-int main() {
+#include <tuple>
+#include <vector>
+#include <cctype>
+#include "utils.hpp"
+
+using namespace std;
+
+enum Opt {
+    Cin,
+    Cout,
+    Decompress,
+    Recursion,
+    CompressionLevel,
+    Test,
+    BadOption,
+    Count
+
+};
+
+enum class Algorithm{
+    LZW,
+    Huffman
+};
+
+void check_logics_opts(vector<bool> &opt) {
+    if (opt[Opt::BadOption]) return;
+    if (opt[Opt::Recursion]) {
+        if (opt[Opt::Cin] || opt[Opt::Cout]) {
+            opt[Opt::BadOption] = true;
+        }
+    }
+}
+
+tuple<vector<bool>, vector<string>> parse_opt(int argc, char *argv[]) {
+    vector<bool> opt(Opt::Count, false);
+    vector<string> args(2);
+    int real_args = 0;
+    for (int i = 1; i < argc; ++i) {
+        string cur_opt = argv[i];
+        if (cur_opt[0] == '-') {
+            if (cur_opt.size() > 2) {
+                opt[Opt::BadOption] = true;
+            } else if (cur_opt.size() == 1) {
+                opt[Opt::Cin] = true;
+            } else {
+                switch (cur_opt[1]) {
+                    case 'c':
+                        opt[Opt::Cout] = true;
+                        break;
+                    case 'd':
+                        opt[Opt::Decompress] = true;
+                        break;
+                    case 'r':
+                        opt[Opt::Recursion] = true;
+                        break;
+                    case 't':
+                        opt[Opt::Test] = true;
+                        break;
+                    default:
+                        if (isdigit(cur_opt[1])) {
+                            opt[Opt::CompressionLevel] = true;
+                            args[1] = cur_opt[1];
+                        } else {
+                            opt[Opt::BadOption] = true;
+                        }
+                }
+            }
+
+        } else {
+            if (real_args >= 1) {
+                opt[Opt::BadOption] = true;
+                continue;
+            }
+            args[real_args] = argv[i];
+            ++real_args;
+        }
+    }
+    if (real_args != 1) opt[Opt::BadOption] = true;
+    check_logics_opts(opt);
+    return {opt, args};
+}
+
+void one_file(vector<bool> &opt, vector<string> arg) {
+    if (opt[Opt::Decompress]) {
+        //TODO
+    } else {
+        if (opt[Opt::Cin] || (opt[Opt::CompressionLevel] && arg[1][0] <= '5')) {
+            if (opt[Opt::Cin]) {
+                if (opt[Opt::Cout]){
+                    write_raw_var(cout, static_cast<uint8_t>(Algorithm::LZW));
+                    LZW::encode(std::cin, cout);
+                } else {
+                    std::ofstream output("input.zipped", std::ios::out | std::ios::binary);
+                    write_raw_var(output, static_cast<uint8_t>(Algorithm::LZW));
+                    LZW::encode(std::cin, output);
+                }
+            } else {
+                if (opt[Opt::Cout]){
+                    std::ifstream input(arg[0]);
+                    write_raw_var(cout, static_cast<uint8_t>(Algorithm::LZW));
+                    LZW::encode(input, cout);
+                } else {
+                    std::ifstream input(arg[0]);
+                    std::ofstream output(arg[0] + ".zipped", std::ios::out | std::ios::binary);
+                    write_raw_var(output, static_cast<uint8_t>(Algorithm::LZW));
+                    LZW::encode(input, output);
+                }
+            }
+        } else {
+            if (opt[Opt::Cout]){
+                write_raw_var(cout, static_cast<uint8_t>(Algorithm::Huffman));
+                Huffman::encode(arg[0], cout);
+            } else {
+                std::ofstream output(arg[0] + ".zipped", std::ios::out | std::ios::binary);
+                write_raw_var(output, static_cast<uint8_t>(Algorithm::Huffman));
+                Huffman::encode(arg[0], output);
+            }
+        }
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+    auto[opt, arg] = parse_opt(argc, argv);
+    if (opt[Opt::Cin] || (Opt::CompressionLevel && arg[1][0] <= '5'))
+
+    /*
     std::string comand;
     std::cin >> comand;
     if (comand == "compress") {
-        /*
-      std::string s;
-    std::stringstream ss;
-    std::cin >> s;
-    ss << s;
-    LZW::encode(ss, std::cout);
-    std::cout << '\n';
-    return 0;
-     */
+
         std::string filename, to_save;
         std::cin >> filename >> to_save;
         std::ifstream i(filename);
@@ -33,5 +148,5 @@ int main() {
         return 0;
     } else {
         return 1;
-    }
+    }*/
 }
